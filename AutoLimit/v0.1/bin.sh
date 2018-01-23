0a16891f391a4d6c2f96a52f83d4e9bd75bbd76a
Thnineer <2388997861@ntua.edu.gr>
2018-01-23 13:41:59 +0800

    First Version

---
+++ AutoLimit/v0.1/bin.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+CPU=`ps aux | grep "qemu-kvm"| sed '/grep/d' | awk '{print $3}' | grep -wf <(seq 100 400)`
+CPUS=`ps aux | grep "qemu-kvm| sed '/grep/d' "| awk '{print $3}' | grep -wf <(seq 100 400) | wc -l`
+for (( i=1; i<$[${CPUS}+1]; i++ ));
+do
+CPU[$i]=`echo "${CPU}" | sed -n ''${i}'p'`;
+PID[$i]=`ps aux | grep "qemu-kvm" | grep "${CPU[$i]}" | sed '/grep/d' | awk '{print $2}' | sed -n ''${i}'p'`;
+if [[ ! -n "${PID[$i]}" ; then
+	echo -e "Now is no over."
+else
+	if [[ ! -e "${PID[$i]}-qemu-kvm.sh" ; then
+	  cp /etc/local/autolimit/limit/default-example.sh /etc/local/autolimit/limit/${PID[$i]}-qemu-kvm.sh
+	  sed -i 's/examplea/'${PID[$i]}'/' /etc/local/autolimit/limit/${PID[$i]}-qemu-kvm.sh
+	  echo "bash test/${PID[$i]}-qemu-kvm.sh -t 1" /etc/local/autolimit/limit/main.sh
+	else 
+	  echo "Exist!"
+	fi
+fi
+done
---
+++ AutoLimit/v0.1/limit/default-example.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+while [[ $# -ge 1 ]]; do
+    case $1 in
+	-t|--time)
+	 shift
+	 times="$1"
+	 ;;
+	*)
+	 echo "Error!"
+	 exit 1;
+	 ;;
+    esac
+done
+
+BinPath="/etc/local/autolimit/limit/"
+PID="examplea"
+Totaltimes="24"
+MaxCPU="100"
+
+if (( "$times" < "$Totaltimes" )) ; then
+	CPUNOW=`ps aux | grep "${PID}" | awk '{print $3}'`
+	CPUNEW=`ps aux | grep "${PID}" | awk '{print $3}' | grep -wf <(seq 100 400)`
+	Date=`date`
+	  if [[ ! -n "${CPUNEW}" ]] ; then
+	    echo -e "[${Date}]: ${times} time(s). Now this process does not exceed the threshold. PID ${PID},CPU ${CPUNOW}" >> ${BinPath}log.txt
+	  else
+	    sed -i 's/'${PID}'-qemu-kvm.sh -t '${times}'/'${PID}'-qemu-kvm.sh -t '$[${times}+1]'/' ${BinPath}main.sh
+	    echo -e "[${Date}]: ${times} time(s). Now it's still beyond the limit. PID ${PID},CPU ${CPUNOW}" >> ${BinPath}log.txt
+	  fi
+else
+	cpulimit -p ${PID} ${MaxCPU} -z &
+	echo -e "[${Date}]: ${times} time(s). Now it's has been restricted. PID ${PID},CPU ${CPUNOW}" >> ${BinPath}log.txt
+fi
