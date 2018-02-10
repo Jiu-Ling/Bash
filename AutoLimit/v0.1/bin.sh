ca36707c50c7a0150aab0f2eb7f3644822cd6fb6
Thnineer <2388997861@ntua.edu.gr>
2018-02-10 12:15:56 +0800

    Fix ]].

--- AutoLimit/v0.1/bin.sh
+++ AutoLimit/v0.1/bin.sh
@@ -7,10 +7,10 @@ for (( i=1; i<$[${CPUS}+1]; i++ ));
 do
 CPU[$i]=`echo "${CPU}" | sed -n ''${i}'p'`;
 PID[$i]=`ps aux | grep "qemu-kvm" | grep "${CPU[$i]}" | sed '/grep/d' | awk '{print $2}' | sed -n ''${i}'p'`;
-if [[ ! -n "${PID[$i]}" ; then
+if [[ ! -n "${PID[$i]}" ]] ; then
 	echo -e "Now is no over."
 else
-	if [[ ! -e "${PID[$i]}-qemu-kvm.sh" ; then
+	if [[ ! -e "${PID[$i]}-qemu-kvm.sh" ]] ; then
 	  cp /usr/local/autolimit/limit/default-example.sh /usr/local/autolimit/limit/${PID[$i]}-qemu-kvm.sh
 	  sed -i 's/examplea/'${PID[$i]}'/' /usr/local/autolimit/limit/${PID[$i]}-qemu-kvm.sh
 	  echo "bash test/${PID[$i]}-qemu-kvm.sh -t 1" /usr/local/autolimit/limit/main.sh
