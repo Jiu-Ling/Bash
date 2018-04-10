982e0119a8e12624484134c51ce28ab61b3baa30
Thnineer <2388997861@ntua.edu.gr>
2018-04-10 19:47:08 +0800

    Update bt-tracker.sh

--- init/bt-tracker.sh
+++ init/bt-tracker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/sbin/service aria2 stop
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
-if [ -z "`grep "bt-tracker" /etc/aria2/aria2c/.conf`" ]; then
+if [ -z "`grep "bt-tracker" /etc/aria2/aria2c.conf`" ]; then
     sed -i '$a bt-tracker='${list} /etc/aria2/aria2c.conf
     echo First use,Preparing......
 else
